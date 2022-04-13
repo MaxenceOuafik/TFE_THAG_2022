cd9f5174ad2449b699b05879f13d85726a960b55
Asrahal <maxence.ouafik@gmail.com>
2022-04-13 19:24:48 +0200

    Changements esthétiques

--- scripts/render_book.R
+++ scripts/render_book.R
@@ -7,10 +7,18 @@ render_book('index.Rmd',
             output_format = 'bookdown::markdown_document2',
             output_dir = 'output/')
 
+read_docx('output/TFE.docx') %>%
+  body_replace_all_text("(PART) ", "", fixed = T) %>%
+  print(target = 'output/TFE1.docx')
+
 read_docx(path = 'files/cover-page.docx') %>%
   body_add_break(pos = 'after') %>%
-  body_add_docx(src = 'output/TFE.docx') %>%
+  body_add_docx(src = 'output/TFE1.docx') %>%
   print(target = 'output/initiation_THAG_MG_TFE.docx')
 
-unlink(c('output/TFE.docx', 
+unlink(c('output/TFE.docx',
+         'output/TFE1.docx',
          'output/reference-keys.txt'))
+
+
+
